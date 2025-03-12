a59f3161eedd1bf4e7987534d584ce28fc403eae
NishiOwO <nishi@nishi.boats>
2025-03-12 16:56:42 +0900

    fix

--- mk/p_generic.mk
+++ mk/p_generic.mk
@@ -1,7 +1,9 @@
 # Generic build
 
+TCLTK_VERSION = 8.6
+
 CC = gcc
-CFLAGS = -g -std=c99 `pkg-config --cflags tcl` `pkg-config --cflags tk`
+CFLAGS = -g -std=c99 `pkg-config --cflags tcl 2>/dev/null` `pkg-config --cflags tk 2>/dev/null` `pkg-config --cflags tcl$(TCLTK_VERSION) 2>/dev/null` `pkg-config --cflags tk$(TCLTK_VERSION) 2>/dev/null`
 LDFLAGS =
-LIBS = `pkg-config --libs tcl` `pkg-config --libs tk`
+LIBS = `pkg-config --libs tcl 2>/dev/null` `pkg-config --libs tk 2>/dev/null` `pkg-config --libs tcl$(TCLTK_VERSION) 2>/dev/null` `pkg-config --libs tk$(TCLTK_VERSION) 2>/dev/null`
 BUILD_DEPS = false
